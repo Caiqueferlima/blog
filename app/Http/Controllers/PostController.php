950407c8ea8231596ee8e978fa28084238e8eeeb
Caíque Fernandes <caiquelima116@gmail.com>
2025-08-20 22:18:41 -0300

    pg 6 a 12 do tutorial (só faltam os testes e a atividade)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    public function index() {
-        $post = "Meu Blog Laravel";
-        return view('posts.index', ['post'=>$post]);
+    public function index()
+    {
+        $posts = Post::all();
+        return view('posts.index', ['posts' => $posts]);
+    }
+    public function create()
+    {
+        return view('posts.create');
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+        $post = new Post;
+        $file_name = time() . '.' . request()->image->getClientOriginalExtension();
+        request()->image->move(public_path('images'), $file_name);
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->image = $file_name;
+        $post->save();
+        return redirect()->route('posts.index')->with('success', 'Post createdsuccessfully.');
+    }
+    public function show(Post $post)
+    {
+        return view('posts.show', ['post' => $post]);
     }
 }
